f3b40da5379bcd220a1a7a4780d47247beb05b45
David Whipple <dwhipp88@gmail.com>
2022-07-13 19:59:53 -0600

    added activities section | working on popup

--- tri-health/assets/js/script.js
+++ tri-health/assets/js/script.js
@@ -1,9 +1,22 @@
 // keeps track of current navigation item
-const navItems = document.querySelectorAll(".nav-item")
+const navItems = document.querySelectorAll('.nav-item');
 
-for (let i = 0; i < navItems.length; i++){
-    navItems[i].addEventListener('click', () => {
-        Array.from(navItems, navItem => navItem.classList.remove('current'));
-        navItems[i].classList.add('current');
-    })
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener('click', () => {
+    Array.from(navItems, (navItem) =>
+      navItem.classList.remove('current')
+    );
+    navItems[i].classList.add('current');
+  });
 }
+
+// activities modal
+
+const dialogItem = document.querySelector('dialog-box');
+const dialogImageItem = document.querySelector('.dialog-image');
+const dialogCloseBtn = document
+  .querySelector('.dialog-btn')
+  .addEventListener('click', function () {
+    alert("I'm listening.");
+    dialogItem.classList.add('modal');
+  });
