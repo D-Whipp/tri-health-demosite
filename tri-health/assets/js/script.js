05c19b400b06ff330cc3b6845d1b94b15227b103
Mr Whipp <dwhipp88@gmail.com>
2022-07-14 01:00:52 -0600

    working on modal login in script.js

--- tri-health/assets/js/script.js
+++ tri-health/assets/js/script.js
@@ -11,12 +11,30 @@ for (let i = 0; i < navItems.length; i++) {
 }
 
 // activities modal
+// now i need an image selector
+// what I'm trying to do is listen to 
+// every item in the activities container
+// images ul li's and listen for a click
+// upon the click event i want to take the
+// clicked image and display it in a modal
+const imageItems = document.querySelectorAll('.image-item');
 
-const dialogItem = document.querySelector('dialog-box');
+for (let i = 0; i < imageItems.length; i++) {
+  imageItems[i].addEventListener('click', () => {
+    Array.from(imageItems, (imageItem) =>
+      imageItem.classList.add('modal')
+    );
+    imageItems[i].classList.add('modal');
+   })
+}
+
+// grabs the modal and makes it disappear
+const dialogItem = document.querySelector('.dialog-box');
 const dialogImageItem = document.querySelector('.dialog-image');
 const dialogCloseBtn = document
   .querySelector('.dialog-btn')
   .addEventListener('click', function () {
-    alert("I'm listening.");
+    // alert("I'm listening.");
+    // console.log(dialogItem);
     dialogItem.classList.add('modal');
   });
