8d9c79015879e9a69eb9b25ed521b04ede582c8f
Mr Whipp <dwhipp88@gmail.com>
2022-07-13 23:22:33 -0600

    removed outdated directory

--- double-health/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const navItems = document.querySelectorAll(".nav-item")
-
-for (let i = 0; i < navItems.length; i++){
-    navItems[i].addEventListener('click', () => {
-        Array.from(navItems, navItem => navItem.classList.remove('current'));
-        navItems[i].classList.add('current');
-    })
-}
