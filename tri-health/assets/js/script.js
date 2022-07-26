99a0505e2232471bde8e36e124400a5a0022f21a
David Whipple <dwhipp88@gmail.com>
2022-07-25 19:08:36 -0600

    added node nodules

--- tri-health/assets/js/script.js
+++ tri-health/assets/js/script.js
@@ -1,3 +1,5 @@
+import 'dotenv/config' 
+
 // keeps track of current navigation item
 const navItems = document.querySelectorAll('.nav-item');
 
@@ -9,39 +11,3 @@ for (let i = 0; i < navItems.length; i++) {
     navItems[i].classList.add('current');
   });
 }
-
-// I can simply use bootstrap's modal
-// const modal = document.querySelector('#modal');
-// const openModal = document.querySelector('#open-button');
-// const closeModal = document.querySelector('.close-button');
-
-// openModal.addEventListener('click', () => {
-//   modal.showModal();
-// });
-
-// activities modal
-// now i need an image selector
-// what I'm trying to do is listen to
-// every item in the activities container
-// images ul li's and listen for a click
-// upon the click event i want to take the
-// clicked image and display it in a modal
-// const imageItems = document.querySelectorAll('.image-item');
-
-// for (let i = 0; i < imageItems.length; i++) {
-//   imageItems[i].addEventListener('click', () => {
-//     Array.from(imageItems, (imageItem) =>
-//       imageItem.classList.add('modal')
-//     );
-//     imageItems[i].classList.add('modal');
-//    })
-// }
-
-// grabs the modal and makes it disappear
-// const dialogItem = document.querySelector('.dialog-box');
-// const dialogImageItem = document.querySelector('.dialog-image');
-// const dialogCloseBtn = document
-//   .querySelector('.dialog-btn')
-//   .addEventListener('click', function () {
-//     dialogItem.classList.add('modal');
-//   });
