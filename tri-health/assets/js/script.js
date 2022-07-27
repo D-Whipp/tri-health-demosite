62d6e1d1f443f298456867b0b608cd0ae64b7e47
David Whipple <dwhipp88@gmail.com>
2022-07-27 15:25:03 -0600

    added current date functionality to latest news, created css to tablet styles

--- tri-health/assets/js/script.js
+++ tri-health/assets/js/script.js
@@ -1,5 +1,8 @@
-import 'dotenv/config' 
+// import 'dotenv/config';
 
+// ***************************
+// navigation section
+// ***************************
 // keeps track of current navigation item
 const navItems = document.querySelectorAll('.nav-item');
 
@@ -11,3 +14,26 @@ for (let i = 0; i < navItems.length; i++) {
     navItems[i].classList.add('current');
   });
 }
+
+// ***************************
+// more-info section
+// ***************************
+// displays current date
+
+function createDate(input) {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+
+  today = mm + '/' + dd + '/' + yyyy;
+  return (input.innerHTML = today);
+}
+
+const dateOneNode = document.getElementById('date-one');
+const dateTwoNode = document.getElementById('date-two');
+const dateThreeNode = document.getElementById('date-three');
+
+createDate(dateOneNode);
+createDate(dateTwoNode);
+createDate(dateThreeNode);
